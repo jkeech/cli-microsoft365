f5ecaf2f32e3588e2d3fa43f0d4f32aa7667f056
Waldek Mastykarz <waldek@mastykarz.nl>
2020-08-08 20:23:28 +0200

    Adds version command

---
+++ src/m365/commands/version.ts
@@ -0,0 +1,32 @@
+import commands from './commands';
+import AnonymousCommand from '../base/AnonymousCommand';
+const packageJSON = require('../../../package.json');
+
+const vorpal: Vorpal = require('../../vorpal-init');
+
+class VersionCommand extends AnonymousCommand {
+  public get name(): string {
+    return commands.VERSION;
+  }
+
+  public get description(): string {
+    return 'Shows CLI for Microsoft 365 version';
+  }
+
+  public commandAction(cmd: CommandInstance, args: {}, cb: (err?: any) => void): void {
+    cmd.log(`v${packageJSON.version}`);
+    cb();
+  }
+
+  public commandHelp(args: any, log: (help: string) => void): void {
+    log(vorpal.find(commands.STATUS).helpInformation());
+    log(
+      `  Examples:
+  
+    Show the version of CLI for Microsoft 365
+      ${commands.STATUS}
+`);
+  }
+}
+
+module.exports = new VersionCommand();
